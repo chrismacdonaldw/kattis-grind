9ad666a3579d13300b14be6aad7410b69f6d8b03
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2019-11-18 19:47:30 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,6 +4,12 @@ using namespace std;
 #define ll long long
 #define ull unsigned long long
 
+void cinsync() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+}
+
 int main() {
+    cinsync();
     
 }
