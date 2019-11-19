ec9f60970c83eed09e46dd3b45c4543c98daf58d
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2019-11-19 15:05:00 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,16 +1,13 @@
 #pragma GCC optimize("Ofast","unroll-loops")
 #pragma GCC target("avx2,fma")
+
 #include <bits/stdc++.h>
 using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
 #define ll long long
 #define ull unsigned long long
 
-void cinsync() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-}
-
 int main() {
-    cinsync();
     
 }
